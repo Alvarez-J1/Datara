04a080bb9912b4ad9af85375442ca0548339d637
Joel Alvarez <Joel124@outlook.es>
2026-05-27 10:47:10 -0400

    fixed bug after signing in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-"use client";
-
-import Dashboard from "./dashboard/page";
-import scss from "./Home.module.scss";
-import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  const { data: session } = useSession();
-
-  return (
-      <main className={scss.main} style={{
-            padding: session ? "80px 0 0 0" : 0,
-          }}>
-   <Dashboard />
-   </main>
-)
+  redirect("/dashboard");
 }
